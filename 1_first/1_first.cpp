f45bb83f068c75ba44a81053fece8d0862babb83
CPU_Code <923992029@qq.com>
2020-05-28 21:27:59 +0800

    第一个代码 输出信息

---
+++ 1_first/1_first.cpp
@@ -0,0 +1,29 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-05-28 21:08:19
+ * @LastEditTime: 2020-05-28 21:13:28
+ * @FilePath: \ccourse\1_first\1_first.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+
+#include <iostream>                           // 预处理器编译指令
+
+int main()                                    // 函数头
+{                                             // 函数体开始
+    using namespace std;                      // 编译指令
+
+    cout << "My first code ";  // 输出信息
+    cout << endl;                             // 换行
+    cout << "换行成功 " << endl;   // more output
+
+    // cout << "Press any key to continue." <<endl;
+	// cin.get(); 
+
+    return 0;                                 // terminate main()
+}       // 结束
+
+/* 运行结果：
+My first code
+换行成功
+*/
