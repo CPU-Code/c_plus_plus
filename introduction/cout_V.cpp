e02e9320d523405b307b1909be61cfb95fbb58ff
CPU_Code <923992029@qq.com>
2020-06-14 22:27:50 +0800

    输出v

---
+++ introduction/cout_V.cpp
@@ -0,0 +1,34 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-14 16:04:02
+ * @LastEditTime: 2020-06-14 16:04:25
+ * @FilePath: \ccourse\introduction\cout_V.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+#include <iostream>
+
+using namespace std;
+
+/**
+题目描述
+每个人都想成为大V (VIP：Very Important Person)，但要一点一点积累才行，
+先从小v做起。要求输出由小写字母v组成的大V。
+输入描述:
+无
+输出描述:
+v   v
+
+ v v
+
+  v
+
+备注:
+换行使用转义字符‘\n’
+ */
+int main()
+{
+    cout << "v   v" << endl;
+    cout << " v v " << endl;
+    cout << "  v  " <<endl;
+}
