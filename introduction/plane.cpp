6806a852b8c80e47143bdcaee071f6523594ae25
CPU_Code <923992029@qq.com>
2020-06-14 22:28:35 +0800

    小飞机

---
+++ introduction/plane.cpp
@@ -0,0 +1,36 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-14 17:00:50
+ * @LastEditTime: 2020-06-14 17:01:56
+ * @FilePath: \ccourse\introduction\plane.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+/**
+题目描述
+KiKi学会了printf在屏幕输出信息，他想输出一架小飞机。请帮他编写程序输出这架小飞机。
+输入描述:
+无
+输出描述:
+     **     
+     **     
+************
+************
+    *  *    
+    *  *    
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"     **     "<<endl;
+    cout<<"     **     "<<endl;
+    cout<<"************"<<endl;
+    cout<<"************"<<endl;
+    cout<<"    *  *    "<<endl;
+    cout<<"    *  *    "<<endl;
+ 
+    return 0;
+}
