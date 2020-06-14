0ecc57f5f3b9ef5d7d2cdf8cc9ef1e86b6f60d9e
CPU_Code <923992029@qq.com>
2020-06-14 22:29:01 +0800

    输出信息

---
+++ introduction/printf.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-14 15:52:30
+ * @LastEditTime: 2020-06-14 15:52:55
+ * @FilePath: \ccourse\introduction\printf.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+
+/**
+题目描述
+于老师经常告诉我们“学习编程最好的办法就是上机实践，因为你要对计算机下指令，想让计算机帮你干活，
+就得多和计算机‘交流’，实践才能出真知。”
+输入描述:
+无
+输出描述:
+Practice makes perfect!
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Practice makes perfect!" << endl;
+    
+    return 0;
+}
