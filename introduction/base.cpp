db84f987ee5b9f0741f2cdec25c0de0d0a4b2273
CPU_Code <923992029@qq.com>
2020-06-14 22:09:40 +0800

    缩短二进制

---
+++ introduction/base.cpp
@@ -0,0 +1,36 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-14 17:18:37
+ * @LastEditTime: 2020-06-14 17:19:12
+ * @FilePath: \ccourse\introduction\base.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+
+/**
+题目描述
+我们处理的整数通常用十进制表示，在计算机内存中是以二进制补码形式存储，但通常二进制表示的整数比较长，
+为了便于在程序设计过程中理解和处理数据，通常采用八进制和十六进制，缩短了二进制补码表示的整数，
+但保持了二进制数的表达特点。请输出十进制整数1234对应的八进制和十六进制。
+
+输入描述:
+无
+输出描述:
+十进制整数1234对应的八进制和十六进制（字母大写），用空格分开，并且要求，在八进制前显示前导0，
+在十六进制数前显示前导0X。
+备注:
+printf可以使用使用格式控制串“%o”、“%X”分别输出八进制整数和十六进制整数，
+并使用修饰符“#”控制前导显示
+ */
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "0" << oct << 1234 << " ";
+    cout << "0X" << setiosflags(ios::uppercase) << hex << 1234 << endl;
+    
+    return 0;
+}
