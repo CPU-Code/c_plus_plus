69588e72dde133b170291516c15e0cdb6864e301
CPU_Code <923992029@qq.com>
2020-06-14 22:28:16 +0800

    分而治之

---
+++ introduction/conquer.cpp
@@ -0,0 +1,37 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-14 16:56:21
+ * @LastEditTime: 2020-06-14 16:57:29
+ * @FilePath: \ccourse\introduction\conquer.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+
+/**
+使用平台：C++14(g++5.4)
+
+题目描述
+程序设计有一个重要的思想就是分而治之（Divide and conquer）。所谓“分而治之” 就是把一个复杂的问题按一定的方法分为解决方法相同但规模较小的若干部分，然后逐个解决，分别找出各部分的解，把各部分的解组成整个问题的解。C语言源程序一般包括多个函数，每个函数对应一个大任务分解后的子功能，其中main函数是程序开始执行的入口。main函数一般包括四部分逻辑功能：定义数据、输入数据、处理数据、输出数据。
+
+输入描述:
+无
+输出描述:
+1. Define data;
+2. Input data;
+3. Process data;
+4. Output data.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "1. Define data;" << endl;
+    cout << "2. Input data;" << endl;
+    cout << "3. Process data;" << endl;
+    cout << "4. Output data." << endl;
+    
+    return 0;
+}
