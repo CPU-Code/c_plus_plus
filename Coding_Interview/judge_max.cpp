acb84066269c3eff6f482c7360d1ded32c01759a
CPU_Code <923992029@qq.com>
2020-06-15 13:16:38 +0800

    编程基础 位运算 思维，无判断max

---
+++ Coding_Interview/judge_max.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-15 13:01:38
+ * @LastEditTime: 2020-06-15 13:02:12
+ * @FilePath: \ccourse\Coding_Interview\judge_max.cpp
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+/**
+题目描述
+请编写一个方法，找出两个数字中最大的那个。条件是不得使用if-else等比较和判断运算符。
+
+给定两个int a和b，请返回较大的一个数。若两数相同则返回任意一个。
+
+测试样例：
+1，2
+返回：2
+ */
+
+
